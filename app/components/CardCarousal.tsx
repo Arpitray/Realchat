8cef85ef59969050b988add09363ae16aac5ba68
Arpit <rayarpit72@gmail.com>
2025-11-29 04:18:44 +0530

    final fix 4

--- app/components/CardCarousal.tsx
+++ app/components/CardCarousal.tsx
@@ -7,28 +7,28 @@ export default function CardCarousal() {
         "Simply click “Create New Room” and enter a name for your discussion or project A dedicated realtime space is created instantly no setup, no waiting",
       name: "Step 1: Create a Room",
       designation: "",
-      src: "https://res.cloudinary.com/dsjjdnife/image/upload/v1764365596/mock1_imgupscaler.ai_V1_Fast__2K_tgveb9.png",
+      src: "https://res.cloudinary.com/dsjjdnife/image/upload/v1764367005/ChatGPT_Image_Nov_29_2025_03_26_12_AM_accztf.png",
     },
     {
       quote:
         "Once the room is created, you get a Room Link and a Room ID. Press the copy button and share it with teammates, friends, or collaborators.",
       name: "Step 2: Invite Members",
       designation: "",
-      src: "https://res.cloudinary.com/dsjjdnife/image/upload/v1764366004/mock2_z9w1mh.png",
+      src: "https://res.cloudinary.com/dsjjdnife/image/upload/v1764366111/mock2_jzwwsk.jpg",
     },
     {
       quote:
         "Paste the invite link or Room ID you received, then click Join Room.Instantly enter the shared space no login required, no complexity.",
       name: "Step 3: Join Room",
       designation: "",
-      src: "https://res.cloudinary.com/dsjjdnife/image/upload/v1764367005/ChatGPT_Image_Nov_29_2025_03_26_12_AM_accztf.png",
+      src: "https://res.cloudinary.com/dsjjdnife/image/upload/v1764367005/ChatGPT_Image_Nov_29_2025_03_26_12_AM_accztf",
     },
     {
       quote:
         "Once inside, users can create diagrams, brainstorm ideas, plan workflows, or sketch concepts together in real-time using the interactive whiteboard.",
       name: "Work together visually, instantly",
       designation: "",
-      src: "https://res.cloudinary.com/dsjjdnife/image/upload/v1764367523/lastMock_qysjt7.png",
+      src: "https://res.cloudinary.com/dsjjdnife/image/upload/v1764367523/lastMock_qysjt7",
     },
   ];
   return (
